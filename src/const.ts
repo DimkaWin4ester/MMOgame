f1a500b103eb920580854e3ad593fd4acba4064a
Dmitrii <skeleton007@yandex.ru>
2024-12-08 00:54:12 +0300

    fix1

--- src/const.ts
+++ src/const.ts
@@ -1,3 +1,3 @@
 export const API_URL = import.meta.env.VITE_API_URL;
 export const API_KEY = import.meta.env.VITE_API_KEY;
-export const API_HOST = new URL(API_URL).host;
+export const API_HOST = import.meta.env.VITE_API_HOST;
